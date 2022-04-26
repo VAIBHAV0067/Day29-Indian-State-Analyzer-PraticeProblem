84c731463498ef79f3c4330724fd8810e7630f0a
tutorial6 <manapure.vaibhav542@gmail.com>
2022-04-26 15:52:26 +0530

    Ability for analyser to load Indian States Census Information from a CSV file

---
+++ IndianStateAnalyzer/IndianStateAnalyzer/CensusAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using IndianStateAnalyzer.DTO;
+using Newtonsoft.Json;
+using System.Xml.XPath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateAnalyzer
+{
+    public class CensusAnalyzer
+    {
+        public enum Country
+        {
+            INDIA
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+    }
+}
---
+++ IndianStateAnalyzer/IndianStateAnalyzer/DTO/StateCodeDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateAnalyzer.POCO
+{
+    public class StateCodeDAO
+    {
+        public int serialNumber;
+        public string stateName;
+        public int tin;
+        public string statecode;
+
+        public StateCodeDAO(string v1, string v2, string v3, string v4)
+        {
+            this.serialNumber = Convert.ToInt32(v1);
+            this.stateName = v2;
+            this.tin = Convert.ToInt32(v3);
+            this.statecode = v4;
+
+        }
+    }
+}
